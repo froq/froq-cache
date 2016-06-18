ee3813e29960bcbeb851353f99319f8c4a769219
Kerem Güneş <k-gun@mail.com>
2016-06-19 01:31:55 +0300

    Add Froq\Cache\Agent\AgentInterface object.

---
+++ src/Agent/AgentInterface.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright (c) 2016 Kerem Güneş
+ *     <k-gun@mail.com>
+ *
+ * GNU General Public License v3.0
+ *     <http://www.gnu.org/licenses/gpl-3.0.txt>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+declare(strict_types=1);
+
+namespace Froq\Cache\Agent;
+
+/**
+ * @package    Froq
+ * @subpackage Froq\Cache\Agent
+ * @object     Froq\Cache\Agent\AgentInterface
+ * @author     Kerem Güneş <k-gun@mail.com>
+ */
+interface AgentInterface
+{
+    /**
+     * Set.
+     * @param  string $key
+     * @param  any    $value
+     * @param  int    $ttl
+     * @return bool
+     */
+    public function set(string $key, $value, int $ttl = Agent::DEFAULT_TTL): bool;
+
+    /**
+     * Get.
+     * @param  string $key
+     * @param  any    $valueDefault
+     * @return any
+     */
+    public function get(string $key, $valueDefault = null);
+
+    /**
+     * Delete.
+     * @return bool
+     */
+    public function delete(): bool;
+}
