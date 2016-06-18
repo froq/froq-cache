47b6a9af39e00f72eda73a61d374b38837b4524e
Kerem Güneş <k-gun@mail.com>
2016-06-19 01:21:05 +0300

    Add Froq\Cache\Agent\Agent object.

---
+++ src/Agent/Agent.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Copyright (c) 2016 Kerem Güneş
+ *     <k-gun@mail.com>
+ *
+ * GNU General Public License v3.0
+ *     <http://www.gnu.org/licenses/gpl-3.0.txt>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+declare(strict_types=1);
+
+namespace Froq\Cache\Agent;
+
+/**
+ * @package    Froq
+ * @subpackage Froq\Cache\Agent
+ * @object     Froq\Cache\Agent\Agent
+ * @author     Kerem Güneş <k-gun@mail.com>
+ */
+abstract class Agent
+{
+    /**
+     * Name.
+     * @var string
+     */
+    private $name;
+
+    /**
+     * TTL.
+     * @var int
+     */
+    private $ttl;
+
+    /**
+     * Constructor.
+     * @param string $name
+     * @param int    $ttl
+     */
+    final public function __construct(string $name, int $ttl)
+    {
+        $this->setName($name);
+        $this->setTtl($ttl);
+    }
+
+    /**
+     * Set name.
+     * @param  string $name
+     * @return self
+     */
+    final public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name.
+     * @return string
+     */
+    final public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set TTL.
+     * @param  int $ttl
+     * @return self
+     */
+    final public function setTtl(int $ttl): self
+    {
+        $this->ttl = $ttl;
+
+        return $this;
+    }
+
+    /**
+     * Get TTL.
+     * @return int
+     */
+    final public function getTtl(): int
+    {
+        return $this->ttl;
+    }
+}
