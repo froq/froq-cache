04daa380520817c1709c26600b68eb057f524ea8
Kerem Güneş <qeremy@gmail.com>
2020-10-13 21:44:12 +0300

    CacheFactory: add new feats [getInstance(),getAgentInstance() etc].

--- src/CacheFactory.php
+++ src/CacheFactory.php
@@ -26,7 +26,7 @@ declare(strict_types=1);
 
 namespace froq\cache;
 
-use froq\cache\CacheException;
+use froq\cache\{Cache, CacheException};
 use froq\cache\agent\{AgentInterface, File, Apcu, Redis, Memcached};
 
 /**
@@ -34,85 +34,144 @@ use froq\cache\agent\{AgentInterface, File, Apcu, Redis, Memcached};
  * @package froq\cache
  * @object  froq\cache\CacheFactory
  * @author  Kerem Güneş <k-gun@mail.com>
- * @since   1.0, 4.1 Renamed as CacheFactory.
+ * @since   4.1, 4.3 Renamed as CacheFactory, added new methods.
  * @static
  */
 final class CacheFactory
 {
     /**
      * Instances.
-     * @var array<froq\cache\agent\AgentInterface>
+     * @var array<string, froq\cache\Cache|froq\cache\agent\AgentInterface>
      */
     private static array $instances = [];
 
     /**
-     * Init.
-     * @param  string     $name
-     * @param  array|null $options
-     * @return froq\cache\agent\AgentInterface
-     * @throws froq\cache\CacheException
+     * Instances.
+     * @return array
+     * @since  4.3
      */
-    public static function init(string $name, array $options = null): AgentInterface
+    public static function instances(): array
     {
-        $static = $options['static'] ?? true; // @default
+        return self::$instances;
+    }
 
-        // Return stored instance.
-        if ($static && isset(self::$instances[$name])) {
-            return self::$instances[$name];
+    /**
+     * Init.
+     * @param  string $id
+     * @param  array  $options
+     * @return froq\cache\Cache
+     */
+    public static function init(string $id, array $options): Cache
+    {
+        $key = self::prepareKey('cache', $id);
+
+        // All cache instances are static as default (not like agent instances).
+        if (!isset(self::$instances[$key])) {
+            // Try to get existing agent in agent instances.
+            try {
+                self::$instances[$key] = new Cache($id, [], self::getAgentInstance($options['id']));
+            } catch (CacheException $e) {
+                self::$instances[$key] = new Cache($id, $options);
+            }
         }
 
-        $agent = null;
+        return self::$instances[$key];
+    }
+
+    /**
+     * Get instance.
+     * @param  string $id
+     * @return froq\cache\Cache
+     * @throws froq\cache\CacheException
+     * @since  4.3
+     */
+    public static function getInstance(string $id): Cache
+    {
+        $key = self::prepareKey('cache', $id);
+
+        if (isset(self::$instances[$key])) {
+            return self::$instances[$key];
+        }
+
+        throw new CacheException('No cache initiated with "%s" name, '.
+            'call "%s::init()" to initiate first', [$id, self::class]);
+    }
+
+    /**
+     * Init agent.
+     * @param  string $id
+     * @param  array  $options
+     * @return froq\cache\agent\AgentInterface
+     * @throws froq\cache\CacheException
+     * @since  4.3
+     */
+    public static function initAgent(string $id, array $options): AgentInterface
+    {
+        $key = self::prepareKey('agent', $id);
+
+        $static = $options['static'] ??= (bool) ($options['static'] ?? true); // @default
+
+        // Return stored instance.
+        if ($static && isset(self::$instances[$key])) {
+            return self::$instances[$key];
+        }
+
         switch (strtolower($name)) {
             case AgentInterface::FILE:
-                $agent = new File($options);
+                $agent = new File($id, $options);
                 break;
             case AgentInterface::APCU:
-                $agent = new Apcu();
+                $agent = new Apcu($id, $options);
                 break;
             case AgentInterface::REDIS:
-                $agent = new Redis();
+                $agent = new Redis($id, $options);
                 break;
             case AgentInterface::MEMCACHED:
-                $agent = new Memcached();
+                $agent = new Memcached($id, $options);
                 break;
             default:
                 throw new CacheException('Unimplemented agent name "%s" given', [$name]);
         }
 
-        // Set possible options.
-        isset($options['host']) && $agent->setHost($options['host']);
-        isset($options['port']) && $agent->setPort($options['port']);
-
-        // Set default ttl if provided.
-        if (isset($options['ttl'])) {
-            $agent->setTtl($options['ttl']);
-        }
-
-        // Connect etc.
+        // Connect etc (@see AgentInterface.init()).
         $agent->init();
 
-        // Store.
+        // Store instance.
         if ($static) {
-            self::$instances[$name] = $agent;
+            self::$instances[$key] = $agent;
         }
 
         return $agent;
     }
 
     /**
-     * Get instance.
-     * @param  string $name
+     * Get agent instance.
+     * @param  string $id
      * @return froq\cache\agent\AgentInterface
      * @throws froq\cache\CacheException
-     * @since  4.1
+     * @since  4.3
      */
-    public static function getInstance(string $name): AgentInterface
+    public static function getAgentInstance(string $id): AgentInterface
     {
-        if (isset(self::$instances[$name])) {
-            return self::$instances[$name];
+        $key = self::prepareKey('agent', $id);
+
+        if (isset(self::$instances[$key])) {
+            return self::$instances[$key];
         }
 
-        throw new CacheException('No agent initiated as static such "%s", call "%s::init()" '.
-            'with static=true option to initiate first', [$name, self::class]);
+        throw new CacheException('No cache agent initiated with "%s" id as static, '.
+            'call "%s::initAgent()" with static=true option to initiate first', [$id, self::class]);
+    }
+
+    /**
+     * Key.
+     * @param  string $base
+     * @param  string $id
+     * @return string
+     * @since  4.3
+     */
+    private static function prepareKey(string $base, string $id): string
+    {
+        return $base .'@'. trim($id);
     }
 }
